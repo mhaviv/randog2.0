ca81076f230cf7fc8d72fd5b6b1c83eaea6638dc
Michael <mhaviv18@gmail.com>
2019-11-12 18:11:15 -0500

    Initial Commit

---
+++ Randog-2.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Randog-2.0
+//
+//  Created by Michael Haviv on 11/12/19.
+//  Copyright © 2019 Michael Haviv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
